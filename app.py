bea1fedd87c8980c49f78402e9cb2a19cbb26628
AdityaAjithKumar <120290212+AdityaAjithKumar@users.noreply.github.com>
2025-05-05 18:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def get_languages():
     return jsonify({str(id): lang["name"] for id, lang in languages.items()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
